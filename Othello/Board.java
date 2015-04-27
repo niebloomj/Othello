aad188cad26f07199e2f6932788b1ac75fdf7ffd
Jacob Niebloom <niebloomj@gmail.com>
2015-04-27 17:04:08 -0400

    update

--- Othello/Board.java
+++ Othello/Board.java
@@ -33,11 +33,55 @@ public class Board {
 
 	public void move(byte x, byte y) {
 		board[x][y] = turn;
-		if (turn == 1)
-			turn = -1;
-		else if (turn == -1)
-			turn = 1;
+		Update(x, y);
+		//player 1
+		// if (turn == 1)
+		// turn = -1;
+		//player -1
+		// else if (turn == -1)
+		// turn = 1;
 		prevMove = new byte[] {x, y};
+	}
+
+	/*
+	 * By using the canfilp and dofilp method to
+	 * update the board.
+	 */
+	public void Update(byte X, byte Y) {
+		int i, j;
+		byte[][] state = this.board;
+		if (X < 0) return; /* pass move */
+		if (state[X][Y] != 0) {
+			print();
+			// printboard(state, turn, turn, X, Y);
+			System.out.println("Illegal move");
+		}
+		state[X][Y] = turn;
+		for (i = -1; i <= 1; i++)
+			for (j = -1; j <= 1; j++)
+				if ((i != 0 || j != 0) && CanFlip(X, Y, i, j))
+					DoFlip(X, Y, i, j);
+	}
+
+	public void DoFlip(int X, int Y, int dirX, int dirY) {
+		byte[][] state = this.board;
+		while (X + dirX < 8 && X + dirX >= 0 && Y + dirY < 8 && Y + dirY >= 0 && state[X + dirX][Y + dirY] == -turn) {
+			X = X + dirX; Y = Y + dirY;
+			state[X][Y] = turn;
+		}
+	}
+
+	public boolean CanFlip(int X, int Y, int dirX, int dirY) {
+		byte[][] state = this.board;
+		boolean capture = false;
+		while (X + dirX < 8 && X + dirX >= 0 && Y + dirY < 8 && Y + dirY >= 0 && state[X + dirX][Y + dirY] == -turn) {
+			X = X + dirX; Y = Y + dirY;
+			capture = true;
+		}
+		if (capture == false) return false;
+		if (X + dirX < 8 && X + dirX >= 0 && Y + dirY < 8 && Y + dirY >= 0 && state[X + dirX][Y + dirY] == turn)
+			return true;
+		else return false;
 	}
 
 	public ArrayList<byte[]> getLegalMoves() {
